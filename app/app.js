70c878c149ac719818383ab216a0f3b729f4d3eb
hreighard <hpreighard@gmail.com>
2015-10-20 01:47:50 -0500

    FINALLY FREAKING WORKING HOLY BALLS

--- app/app.js
+++ app/app.js
@@ -1,12 +1,17 @@
-'use strict';
-
 // Declare app level module which depends on views, and components
-var shopTracker = angular.module('shopTracker', [
-  'shopControllers', 
+angular.module('shopTracker', [])
+.controller('MainControl', function($scope, $http){
+	$http.get('https://shoptracker-api.azurewebsites.net/members').then(function(resp){
+		$scope.members=resp.data;
+	}, function(err){
+		console.error('ERR', err);
+	})
+})
+  /*'shopControllers', 
   'shopServices', 
   'ngRoute',
-  'ngResource'
-]);
+  'ngResource'*/
+
   /*
 shopTracker.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
