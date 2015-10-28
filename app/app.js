ed51cf8e0778efa033e3d6e71d95a5e1c120f71f
hreighard <hpreighard@gmail.com>
2015-10-28 16:22:58 -0500

    multi-controller setup

--- app/app.js
+++ app/app.js
@@ -1,11 +1,26 @@
 // Declare app level module which depends on views, and components
 angular.module('shopTracker', [])
-.controller('MainControl', function($scope, $http){
+.controller('MemberControl', function($scope, $http){
 	$http.get('https://shoptracker-api.azurewebsites.net/members').then(function(resp){
 		$scope.members=resp.data;
 	}, function(err){
 		console.error('ERR', err);
 	})
+})
+.controller('chDeviceControl', function($scope, $http){
+  $http.get('https://shoptracker-api.azurewebsites.net/checkin-devices').then(function(resp){
+    $scope.chdevice=resp.data;
+  }, function(err){
+    console.err('ERR', err);
+  })
+})
+
+.controller('SubgroupControl', function($scope, $http){
+  $http.get('https://shoptracker-api.azurewebsites.net/subgroups').then(function(resp){
+    $scope.subgroups=resp.data;
+  }, function(err){
+    console.err('ERR', err);
+  })
 })
   /*'shopControllers', 
   'shopServices', 
